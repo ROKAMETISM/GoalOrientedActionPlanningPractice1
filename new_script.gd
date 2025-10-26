b2bd3686088a1c3fa248bbb9eabebd5304bba304
ROJS <mskim829@naver.com>
2025-10-26 23:18:41 +0900

    Init

---
+++ new_script.gd
@@ -0,0 +1,18 @@
+extends Node
+
+enum my_state {
+	Idle,
+	Move,
+	Test
+}
+
+var direction : int
+
+func _ready() -> void:
+	if not is_moving() : direction = 0
+
+func get_state_name(state : my_state)->String:
+	return my_state.keys().get(state)
+
+func is_moving()->bool:
+	return false
